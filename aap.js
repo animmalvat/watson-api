725c8363f53b618f6854fd829e475cb3d07a8aed
hardikphp <hardik.mca08@gmail.com>
2019-08-06 12:48:57 +0530

    changes after a while

--- aap.js
+++ aap.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var as = require("./as");
 var getting_data = require("./getting_data");
 var session = require("express-session");
+var passport = require("passport");
 
 const app = express();
 
@@ -16,7 +17,27 @@ app.use(
         extended: false
     })
 );
-app.use(session({ secret: "indiaisgreat" }));
+app.use(
+    session({
+        resave: true,
+        saveUninitialized: true,
+        secret: "XCR3rsasa%RDHHH",
+        cookie: { maxAge: 60000 }
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Origin", "http://192.168.1.53:3000");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Request-With, Content-Type, Accept"
+    );
+    next();
+});
 
 app.use("/", router);
 
--- as.js
+++ as.js
@@ -7,104 +7,67 @@ var assistant = new AssistantV1({
     version: "2019-02-28"
 });
 
-var session;
+var session = null;
 
 exports.watson_api = async (req, res) => {
     //fetching any live values that is needed
     session = req.session;
-    if (session.created != undefined) {
-        session.context = {};
-        session.intent = {};
-        session.stock_data = {};
-        session.isShowStock = false;
-        session.isSellStock = false;
-        session.isBuyStock = false;
+    session.output = "";
+    if (!session.created) {
         session.created = true;
+        session.context = {};
     }
 
-    await fetchingLiveValues();
-
-    async function fetchingLiveValues() {
-        //getting all the details from the api for show stock api
-        if (session.isShowStock) {
-            console.log("showing stock details");
-            session.isShowStock = false;
-            await getting_data.get_price(req.body.input).then(value => {
-                console.log("value success : ", value.success);
-                if (value.success == "true") {
-                    session.context["stock_low"] = value.latestPrice;
-                    session.context["stock_high"] = value.latestPrice;
-                    session.context["stock_close"] = value.latestPrice;
-                    session.context["stock_volume"] = value.latestPrice;
-                    session.context["stock_available"] = true;
-                } else {
-                    session.context["stock_available"] = false;
-                }
-            });
-        }
-
-        //setting up the stock price using api for buying dialouges
-        if (session.isBuyStock) {
-            console.log("getting live data for buying");
-            session.isBuyStock = false;
-            await getting_data.get_price(req.body.input).then(value => {
-                //got the price from live api
-                if (value.success == "true") {
-                    session.context["stock_value"] = value.latestPrice;
-                    session.context["stock_available"] = true;
-                } else {
-                    session.context["stock_available"] = false;
-                }
-            });
-        }
-
-        //setting up the stock price using api for selling dialouges
-        if (session.isSellStock) {
-            console.log("getting data for sell stock");
-            session.isSellStock = false;
-
-            await getting_data.get_price(req.body.input).then(value => {
-                //got the price from live api
-                if (value.success == "true") {
-                    session.context["stock_value"] = value.latestPrice;
-                    session.context["stock_available"] = true;
-                } else {
-                    session.context["stock_available"] = false;
-                }
-            });
-        }
-    }
-
-    assistant
+    await assistant
         .message({
-            input: { text: req.body.input.replace(/(\r\n|\n|\r)/gm, "") }, //message input by the user
-            workspace_id: "867868bf-d9bd-4d84-8fa2-486fce2d79e8", //workspace id
+            input: { text: req.body.input }, //message input by the user
+            workspace_id: "e3083f39-f7fd-44fc-94e8-600e010d5cf8", //workspace id
             context: session.context //previous context
         })
-        .then(result => {
+        .then(async result => {
             session.context = result.context; //context given by the watson
+            console.log(session.context);
 
-            if (result.intents[0] != undefined) {
-                console.log("intent : ", result.intents[0].intent);
-                if (result.intents[0].intent == "show_stock_details") {
-                    session.isShowStock = true;
-                    console.log("show stock : ", session.isShowStock);
-                }
-                // checking if the user is interested in buying stocks
-                if (result.intents[0].intent == "buy_stock") {
-                    session.isBuyStock = true;
-                    console.log("buy stock : ", session.isBuyStock);
-                }
+            if (result.context.action != undefined) {
+                if (result.context.action == "multiply") {
+                    var n = Number(result.context.first_number);
+                    session.context.multiplied = n * 9;
+                    //resetting
+                    session.context.action = "";
 
-                // checking if the user is interested in selling stocks
-                if (result.intents[0].intent == "sell_stock") {
-                    session.isSellStock = true;
-                    console.log("sell stock : ", session.isSellStock);
+                    //calling watson
+                    await assistant
+                        .message({
+                            input: { text: "test sample" }, //message input by the user
+                            workspace_id:
+                                "e3083f39-f7fd-44fc-94e8-600e010d5cf8", //workspace id
+                            context: session.context //previous context
+                        })
+                        .then(data => {
+                            session.context = data.context;
+                            console.log(session.context);
+                            session.context.multiplied = "";
+                            session.context.action = "";
+
+                            session.output =
+                                session.output + data.output.generic[0].text;
+                            console.log(session.output);
+                        });
                 }
             }
-            res.send(result.output.generic[0].text);
+            if (result.output.generic[0] != undefined) {
+                session.output += result.output.generic[0].text;
+            } else {
+                session.output = "Sorry, some error occured";
+            }
+
+            res.send(session.output);
+            session.output = "";
         })
         .catch(err => {
             console.log(err);
+            res.send("Sorry, let's start again.");
         });
+
+    console.log("------------------------------------------------");
 };
